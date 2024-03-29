d26531930fd009acc0402f97642719df5a5d82b5
Gabe Serna <161985200+gabe-serna@users.noreply.github.com>
2024-03-29 18:04:26 -0400

    Delete weight calculator.py

--- weight calculator.py
+++
@@ -1,8 +0,0 @@
-response = input("Please input your weight: ")
-unit = input("Please selected which unit to convert to: \nlbs (L)\nkg (K)\n")
-if unit.upper() == "L":
-    outputLbs = int(response) * 2.205
-    print(f"Your weight is {str(round(outputLbs, 2))} lbs")
-elif unit.upper() == "K":
-    outputKg = int(response) * 0.453
-    print(f"Your WEIGHT is {str(round(outputKg,2))} kg")
