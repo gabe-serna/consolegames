8451a3e76dfe8aaad957ac80cd72484673b8efd1
Gabe Serna <161985200+gabe-serna@users.noreply.github.com>
2024-03-31 08:25:42 -0400

    Add files via upload

---
+++ wordle.py
@@ -0,0 +1,97 @@
+import random
+import sys
+
+from colorama import Fore,init
+init(autoreset=True)
+
+board = [
+    ["_","_","_","_","_"],
+    ["_","_","_","_","_"],
+    ["_","_","_","_","_"],
+    ["_","_","_","_","_"],
+    ["_","_","_","_","_"],
+]
+words = [
+    "apple", "water", "table", "chair", "house", "mouse", "light", "night",
+    "right", "drive", "cream", "dream", "plant", "green", "brown", "black",
+    "white", "watch", "clock", "heart", "music", "beach", "peace", "happy",
+    "smile", "child", "adult", "round", "cheap", "clean", "sharp", "young",
+    "sweet", "quick", "sharp", "earth", "floor", "cloud", "sound", "radio",
+    "money", "power", "train", "river", "honey", "chair", "shelf", "glass",
+    "brush", "knife"
+]
+counter = 0
+word = words[random.randint(0,49)].upper()
+color_reference = {
+    'Fore.GREEN': Fore.GREEN,
+    'Fore.YELLOW': Fore.YELLOW
+}
+
+def start_game():
+    print_board()
+    guess_function()
+
+def print_board():
+    for row in range(0,5):
+        for column in range(0,5):
+            letter = board[row][column]
+            end = "\n" if (column == 4) else ""
+            if letter in word:
+                color = "Fore.GREEN" if letter == word[column] else "Fore.YELLOW"
+                print(color_reference[color] + letter, end=end)
+            else:
+                print(letter,end=end)
+
+
+
+        # column = 0
+        # while column < 4:
+        #     letter = board[row][column]
+        #     if letter in word:
+        #         if letter == word[column]:
+        #             print(Fore.GREEN + letter, end=" ")
+        #         else:
+        #             print(Fore.YELLOW + letter, end=" ")
+        #     else:
+        #         print(letter,end=" ")
+        #     column += 1
+        # # Checking for Column 5 separately to have line indent
+        # letter = board[row][column]
+        # if letter in word:
+        #     if letter == word[column]:
+        #         print(Fore.GREEN + letter)
+        #     else:
+        #         print(Fore.YELLOW + letter)
+        # else:
+        #     print(letter)
+        # row += 1
+
+def guess_function():
+    global counter
+    while counter <= 4:
+        guess = input("Guess a word: ").upper()
+        print("\n")
+        if len(guess) == 5:
+            add_guess_to_board(guess)
+        else:
+            print("Guess must be 5 letters!")
+            print_board()
+            guess_function()
+        if guess == word:
+            if counter == 1:
+                print(f"You have guessed the word in {counter + 1} guess!")
+            else:
+                print(f"You have guessed the word in {counter + 1} guesses!")
+            sys.exit()
+        counter += 1
+    print(f"You have failed to guess the word! The word was {word}")
+
+def add_guess_to_board(guess):
+    x = 0
+    for letter in guess:
+        board[counter][x] = letter
+        x += 1
+    print_board()
+
+
+start_game()
